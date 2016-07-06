00ca07e607fae1daa664708bd0aa4e680db3f026
shtylev <shtylevAA@rambler.ru>
2016-07-06 09:22:55 +0300

    Создан пустой проект

---
+++ AccountingOfSales/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AccountingOfSales.Startup))]
+namespace AccountingOfSales
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
