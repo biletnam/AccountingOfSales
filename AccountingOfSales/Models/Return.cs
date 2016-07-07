2c73bc19fccfed89e2d6cdbc3a5e0441ba41a173
shtylev <shtylevAA@rambler.ru>
2016-07-07 19:24:26 +0300

    Добавление класса Return

---
+++ AccountingOfSales/Models/Return.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingOfSales.Models
+{
+    public class Return
+    {
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
