e21761d5a9cd8c4b8a0d0e700469d6a31fecf837
shtylev <shtylevAA@rambler.ru>
2016-07-07 19:42:00 +0300

    Создание класса IdentityModels

---
+++ AccountingOfSales/Models/IdentityModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingOfSales.Models
+{
+    class SalesDbContext : DbContext
+    {
+        public SalesDbContext(): base("DefaultConnection") { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Provider> Providers { get; set; }
+        public DbSet<TypeProduct> TypeProducts { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Admission> Admissions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<Return> Returns { get; set; }
+
+    }
+}
