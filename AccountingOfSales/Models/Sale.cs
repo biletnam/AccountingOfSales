1881cb4e4bd56513141eb0dbbf5dc113ca3b0523
shtylev <shtylevAA@rambler.ru>
2016-07-07 19:23:24 +0300

    Добавление класса Sale

---
+++ AccountingOfSales/Models/Sale.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingOfSales.Models
+{
+    public class Sale
+    {
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+        public double RetailPrice { get; set; } //розничная цена
+        public double Discount { get; set; }    //скидка
+        public double SalePrice { get; set; }   //цена продажи
+        public DateTime CreateDate { get; set; }
+        public DateTime SaleDate { get; set; }  //дата продажи
+
+        [ScaffoldColumn(false)]
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
