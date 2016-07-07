cb35967fc43364c111db2cd3c9ae96d9b0a38bb2
shtylev <shtylevAA@rambler.ru>
2016-07-07 19:24:45 +0300

    Добавление класса Image

---
+++ AccountingOfSales/Models/Image.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingOfSales.Models
+{
+    public class Image
+    {
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+        public string Extension { get; set; }
+        public string Name
+        {
+            get
+            {
+                return $"{Id}.{Extension}";
+            }
+        }
+    }
+}
