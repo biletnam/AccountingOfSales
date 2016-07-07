cff79d5797b28b54f0e168ccefa16c7dac17ded7
shtylev <shtylevAA@rambler.ru>
2016-07-07 19:24:32 +0300

    Добавление класса Provider

---
+++ AccountingOfSales/Models/Provider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingOfSales.Models
+{
+    public class Provider
+    {
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+        public virtual ICollection<Admission> Admission { get; set; } = new List<Admission>();
+    }
+}
