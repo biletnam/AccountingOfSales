f67559ea3edbbe0a0ff86d9179b1cff1ad7f1625
shtylev <shtylevAA@rambler.ru>
2016-07-13 19:58:16 +0300

    Изменение аттрибутов

--- AccountingOfSales/Models/Product.cs
+++ AccountingOfSales/Models/Product.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace AccountingOfSales.Models
 {
@@ -16,6 +17,7 @@ namespace AccountingOfSales.Models
         [Display(Name = "Наименование")]
         [StringLength(150, ErrorMessage = "Количество символов не должно превышать 150")]
         [RegularExpression(@"[\w\d\sА-яёЁ:!?,.()%-]*", ErrorMessage = "Текст содержит запрещающие символы")]
+        [Remote("CheckName", "Product", ErrorMessage = "Товар с таким наименованием уже имеется")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Поле не должно быть пустым")]
@@ -41,9 +43,10 @@ namespace AccountingOfSales.Models
 
         [Display(Name = "Количество")]
         public int Count { get; set; }
-        
+
+        [Required(ErrorMessage = "Поле не должно быть пустым")]
         [Display(Name = "Розничная цена")]
-        [RegularExpression(@"[\d\s.]*", ErrorMessage = "Текст содержит запрещающие символы")]
+        [RegularExpression(@"[\d.]*", ErrorMessage = "Текст содержит запрещающие символы")]
         [Range(0, 1000000000, ErrorMessage = "Недопустимое число")]
         /// <summary>
         /// Розничная цена
