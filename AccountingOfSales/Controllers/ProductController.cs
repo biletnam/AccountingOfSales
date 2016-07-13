ef79f6a7817ab70980f0d630d013dafe17ab2a8f
shtylev <shtylevAA@rambler.ru>
2016-07-13 19:54:08 +0300

    Реализация загрузки картинки и проверки на совпадение наименования продукта

--- AccountingOfSales/Controllers/ProductController.cs
+++ AccountingOfSales/Controllers/ProductController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using AccountingOfSales.Models;
 using PagedList.Mvc;
 using PagedList;
+using System.IO;
 
 namespace AccountingOfSales.Controllers
 {
@@ -25,21 +26,52 @@ namespace AccountingOfSales.Controllers
                 products = products.Where(n => n.Name.Contains(name)).ToList();
             }
 
-            return View(products.OrderBy(n => n.Name).ToPagedList(pageNumber, pageSize));
+            return View(products.OrderByDescending(n => n.Count).ThenBy(c => c.Name).ToPagedList(pageNumber, pageSize));
         }
 
         public ActionResult Create()
         {
-            SelectList providers = new SelectList(db.Providers.OrderBy(n => n.Name), "Id", "Name");
-            ViewBag.Providers = providers;
+            //SelectList providers = new SelectList(db.Providers.OrderBy(n => n.Name), "Id", "Name");
+            //ViewBag.Providers = providers;
+            //для заполнения выпадающих списков
+            ViewBag.Providers = new SelectList(db.Providers.OrderBy(n => n.Name), "Id", "Name");
+            ViewBag.TypeProducts = new SelectList(db.TypeProducts.OrderBy(n => n.Name), "Id", "Name");
 
             return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Name, Model, Color, Size, RetailPrice, ProviderId, TypeProductId, ImageId")] Product product)
+        public ActionResult Create([Bind(Include = "Name, Model, Color, Size, RetailPrice, ProviderId, TypeProductId, ImageId")] Product product, HttpPostedFileBase image)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                if (image != null)
+                {
+                    Image dbImage = new Image();
+                    dbImage.Extension = Path.GetExtension(image.FileName).TrimStart(new char[] { '.' });
+                    db.Images.Add(dbImage);
+                    db.SaveChanges();
+
+                    image.SaveAs(Server.MapPath("~/Images/" + dbImage.Name));
+
+                    product.Image = dbImage;
+                }
+
+                product.CreateDate = DateTime.Now;
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        public JsonResult CheckName(string Name)
+        {
+            if (db.Products.Any(m => m.Name == Name))
+                return Json(false, JsonRequestBehavior.AllowGet);
+
+            return Json(true, JsonRequestBehavior.AllowGet);
         }
 
         protected override void Dispose(bool disposing)
