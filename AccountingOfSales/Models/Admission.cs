ecc70c95f3365518aba908aabf1bfb42c95f4f62
shtylev <shtylevAA@rambler.ru>
2016-07-29 20:49:36 +0300

    Изменение атрибутов и добавление нового поля Count

--- AccountingOfSales/Models/Admission.cs
+++ AccountingOfSales/Models/Admission.cs
@@ -11,19 +11,37 @@ namespace AccountingOfSales.Models
     {
         [ScaffoldColumn(false)]
         public int Id { get; set; }
+
+        [Display(Name = "Дата создания")]
         public DateTime CreateDate { get; set; }
+
+        [Display(Name = "Дата поступления")]
         /// <summary>
         /// Дата поступления
         /// </summary>
         public DateTime AdmissionDate { get; set; }
+
+        [Display(Name = "Дополнительные расходы")]
+        [RegularExpression(@"[\d.]*", ErrorMessage = "Текст содержит запрещающие символы")]
+        [Range(0, 1000000000, ErrorMessage = "Недопустимое число")]
         /// <summary>
         /// Дополнительные расходы
         /// </summary>
-        public double AdditionalCosts { get; set; }
+        public double? AdditionalCosts { get; set; }
+
+        [Required(ErrorMessage = "Поле не должно быть пустым")]
+        [Display(Name = "Оптовая цена")]
+        [RegularExpression(@"[\d.]*", ErrorMessage = "Текст содержит запрещающие символы")]
+        [Range(0, 1000000000, ErrorMessage = "Недопустимое число")]
         /// <summary>
         /// Оптовая цена
         /// </summary>
         public double TradePrice { get; set; }
+
+        [Required(ErrorMessage = "Поле не должно быть пустым")]
+        [Display(Name = "Количество")]
+        [RegularExpression(@"[\d]*", ErrorMessage = "Вводить разрешено только целые числа")]
+        public int Count { get; set; }
 
         [ScaffoldColumn(false)]
         public int ProductId { get; set; }
