fcffb5d71f32b5b9abb3fe675989360acea4085d
shtylev <shtylevAA@rambler.ru>
2016-08-01 20:45:47 +0300

    Добавление пакета bootstrap-datepicker

---
+++ AccountingOfSales/Models/ViewModel/AdmissionViewModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PagedList.Mvc;
+
+namespace AccountingOfSales.Models.ViewModel
+{
+    public class IndexViewModels
+    {
+        public DateTime FilterDateAdmissionFrom { get; set; }
+        public DateTime FilterDateAdmissionTo { get; set; }
+        public string UserName { get; set; }
+        PagedList.IPagedList<Admission> Admissions { get; set; }
+    }
+}
