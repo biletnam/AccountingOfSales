3ac70346e3fc51176cfbfb919399bc97b8e73b72
shtylev <shtylevAA@rambler.ru>
2016-08-17 21:03:39 +0300

    Добавление аттрибутов для прочих расходов

--- AccountingOfSales/Models/OtherCosts.cs
+++ AccountingOfSales/Models/OtherCosts.cs
@@ -11,9 +11,22 @@ namespace AccountingOfSales.Models
     {
         [ScaffoldColumn(false)]
         public int Id { get; set; }
+
+        [Required]
         public DateTime CreateDate { get; set; }
+
+        [Required(ErrorMessage = "Поле \"Дата расхода\" не может быть пустым")]
         public DateTime CostsDate { get; set; }
+
+        [Display(Name = "Сумма")]
+        [Required(ErrorMessage = "Поле \"Сумма\" не может быть пустым")]
+        [RegularExpression(@"[\d.]*", ErrorMessage = "Сумма содержит запрещающие символы")]
+        [Range(0, 1000000000, ErrorMessage = "Недопустимое число")]
         public double Price { get; set; }
+
+        [Display(Name = "Комментарий")]
+        [StringLength(150, ErrorMessage = "Количество символов не должно превышать 150")]
+        [RegularExpression(@"[\w\d\sА-яёЁ:!?,.()%-]*", ErrorMessage = "Текст содержит запрещающие символы")]
         public string Comment { get; set; }
         /// <summary>
         /// Показывает, что расход был создан при создании поступления
