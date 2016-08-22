de106e997e33da70b652b69a21e541c07f2eaada
shtylev <shtylevAA@rambler.ru>
2016-08-22 21:01:13 +0300

    Добавление класса и реализация метода - возвращает юзера по имени

---
+++ AccountingOfSales/Models/Entities/UserEntities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingOfSales.Models.Entities
+{
+    public class UserEntities
+    {
+        static SalesDbContext db = new SalesDbContext();
+
+        public static User GetUserByName(string name)
+        {
+            return db.Users.Where(n => n.Login == name).FirstOrDefault();
+        }
+    }
+}
