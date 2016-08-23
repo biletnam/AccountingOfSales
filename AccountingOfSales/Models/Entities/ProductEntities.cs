4d9298c78c58ef4d0de98325bcc64bd05bd0a771
shtylev <shtylevAA@rambler.ru>
2016-08-23 17:30:35 +0300

    Добавление класса, с реализацией метода возвращения продукта, по ид

---
+++ AccountingOfSales/Models/Entities/ProductEntities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingOfSales.Models.Entities
+{
+    public class ProductEntities
+    {
+        static SalesDbContext db = new SalesDbContext();
+
+        public static Product GetProductById(int id)
+        {
+            return db.Products.Where(i => i.Id == id).FirstOrDefault();
+        }
+    }
+}
