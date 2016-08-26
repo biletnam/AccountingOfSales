0a98ed6f0014e531a571333c0bd2b9b0823de06b
shtylev <shtylevAA@rambler.ru>
2016-08-26 20:49:01 +0300

    Добавление аттрибутов

--- AccountingOfSales/Models/TypeReturn.cs
+++ AccountingOfSales/Models/TypeReturn.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace AccountingOfSales.Models
 {
@@ -11,7 +12,14 @@ namespace AccountingOfSales.Models
     {
         [ScaffoldColumn(false)]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Поле не должно быть пустым")]
+        [Display(Name = "Наименование")]
+        [StringLength(150, ErrorMessage = "Количество символов не должно превышать 150")]
+        [RegularExpression(@"[\w\d\sА-яёЁ:!?,.()%-]*", ErrorMessage = "Текст содержит запрещающие символы")]
+        [Remote("CheckName", "TypeReturn", ErrorMessage = "Тип возврата с таким наименованием уже имеется", AdditionalFields = "Id")]
         public string Name { get; set; }
+        public bool Archive { get; set; } = false;
         public virtual ICollection<Return> Returns { get; set; } = new List<Return>();
     }
 }
