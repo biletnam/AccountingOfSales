2fde4728bf545492d7a1c30910ac1721df32aa93
shtylev <shtylevAA@rambler.ru>
2016-08-26 20:45:28 +0300

    Удаление условия для даты по и добавляем равно

--- AccountingOfSales/Models/Entities/SalesEntities.cs
+++ AccountingOfSales/Models/Entities/SalesEntities.cs
@@ -17,18 +17,15 @@ namespace AccountingOfSales.Models.Entities
             //находим дату последних 3 месяцев, от текущей, чтобы ограничить поступления 3 последними месяцами
             DateTime last3Months = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day).AddMonths(-3);
 
-            if (filterDateSaleTo != null)
-                filterDateSaleTo = filterDateSaleTo.Value.AddDays(1); //прибавляем к "дате по" 1 день, чтобы дата была включительно
-
             if (filterDateSaleFrom != null && filterDateSaleTo != null)
-                sales = db.Sales.Where(d => d.SaleDate >= filterDateSaleFrom).Where(d => d.SaleDate < filterDateSaleTo).ToList();
+                sales = db.Sales.Where(d => d.SaleDate >= filterDateSaleFrom).Where(d => d.SaleDate <= filterDateSaleTo).ToList();
             else if (filterDateSaleFrom != null && filterDateSaleTo == null)
                 sales = db.Sales.Where(d => d.SaleDate >= filterDateSaleFrom).ToList();
             else if (filterDateSaleFrom == null && filterDateSaleTo != null)
             {
                 //находим дату последних 3 месяцев, от "даты по", чтобы опять же ограничить 3 месяцами
                 DateTime last3MonthsDateTo = new DateTime(filterDateSaleTo.Value.Year, filterDateSaleTo.Value.Month, filterDateSaleTo.Value.Day).AddMonths(-3);
-                sales = db.Sales.Where(d => d.SaleDate >= last3MonthsDateTo).Where(d => d.SaleDate < filterDateSaleTo).ToList();
+                sales = db.Sales.Where(d => d.SaleDate >= last3MonthsDateTo).Where(d => d.SaleDate <= filterDateSaleTo).ToList();
             }
             else
                 sales = db.Sales.Where(d => d.SaleDate >= last3Months).ToList();
