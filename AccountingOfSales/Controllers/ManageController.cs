ae3a976011d57d30932d04e92b79db6d883279a1
shtylev <shtylevAA@rambler.ru>
2016-09-13 18:30:37 +0300

    Добавление контроллера. Реализация изменения пароля

---
+++ AccountingOfSales/Controllers/ManageController.cs
@@ -0,0 +1,57 @@
+﻿using AccountingOfSales.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AccountingOfSales.Models.ViewModel;
+
+namespace AccountingOfSales.Controllers
+{
+    [Authorize]
+    public class ManageController : Controller
+    {
+        SalesDbContext db = new SalesDbContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = db.Users.FirstOrDefault(l => l.Login == model.Login);
+                if (user != null)
+                {
+                    string hashPassword = CryptHelper.CreateHashMD5(model.Password);
+                    user.Password = hashPassword;
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index", "Product");
+                }
+                else
+                    return HttpNotFound();
+            }
+
+            return View(model);
+        }
+
+        public ActionResult EditUser()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
