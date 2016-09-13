34871ee1d4f60a4a5b51ce237a048ec693e3bd80
shtylev <shtylevAA@rambler.ru>
2016-09-13 19:35:24 +0300

    Реализация редактирования пользователя

--- AccountingOfSales/Controllers/ManageController.cs
+++ AccountingOfSales/Controllers/ManageController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using AccountingOfSales.Models.ViewModel;
+using System.Net;
+using System.Web.Security;
 
 namespace AccountingOfSales.Controllers
 {
@@ -43,9 +45,47 @@ namespace AccountingOfSales.Controllers
             return View(model);
         }
 
-        public ActionResult EditUser()
+        public ActionResult EditUser(string Login)
         {
-            return View();
+            if(Login == "")
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            User user = db.Users.FirstOrDefault(l => l.Login == Login);
+
+            if (user != null)
+            {
+                EditUserViewModel userModel = new EditUserViewModel() { Id = user.Id, Login = user.Login, FIO = user.FIO };
+                return View(userModel);
+            }
+            else
+                return HttpNotFound();
+        }
+        [HttpPost]
+        public ActionResult EditUser(EditUserViewModel newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = db.Users.FirstOrDefault(l => l.Id == newUser.Id);
+
+                if (user != null)
+                {
+                    user.FIO = newUser.FIO;
+                    user.Login = newUser.Login;
+                    db.SaveChanges();
+
+                    FormsAuthentication.SetAuthCookie(user.Login, false);
+                    return RedirectToAction("Index", "Product");
+                }
+                else
+                    return HttpNotFound();
+            }
+
+            return View(newUser);
+        }
+
+        public JsonResult CheckLogin(string Login, int? Id)
+        {
+            return Json(!db.Users.Any(m => m.Login == Login && m.Id != Id), JsonRequestBehavior.AllowGet);
         }
 
         protected override void Dispose(bool disposing)
