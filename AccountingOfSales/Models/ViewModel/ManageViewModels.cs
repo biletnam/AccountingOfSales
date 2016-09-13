b899c443ff9088b839ad93a66abbe12a6e4f5642
shtylev <shtylevAA@rambler.ru>
2016-09-13 18:30:49 +0300

    Добавление класса ViewModels

---
+++ AccountingOfSales/Models/ViewModel/ManageViewModels.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace AccountingOfSales.Models.ViewModel
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        public string Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Новый пароль")]
+        [StringLength(10, ErrorMessage = "Пароль должен содержать не менее 1 и не более 10 символов.", MinimumLength = 1)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтверждение пароля")]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class EditUserViewModel
+    {
+
+    }
+}
